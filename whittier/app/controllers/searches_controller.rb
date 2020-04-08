24bbaf4bc2a250319804dc87ed3e6046f04fd9f2
jernber <oraclemars@gmail.com>
2020-04-07 22:10:42 -0500

    created methods and links on nav bar

--- whittier/app/controllers/searches_controller.rb
+++ whittier/app/controllers/searches_controller.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 class SearchesController < ApplicationController
   def index
+    @search = Search.new
   end
+
+  def new
+    @search = Search.create(search_params)
+    redirect_to @search
+  end
+
+  def show; end
 end
