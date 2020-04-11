a71e485636b75b5dafe48abb0cdc512751232086
jernber <oraclemars@gmail.com>
2020-04-11 17:19:47 -0500

    add to cart button functioning

--- whittier/app/controllers/clothing_orders_controller.rb
+++ whittier/app/controllers/clothing_orders_controller.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 class ClothingOrdersController < ApplicationController
+  def create
+    @order = current_order
+    @order_item = @order.clothing_orders.new
+    @order.save
+    session[:order_id] = @order.id
+  end
+
+  private
+
+  def order_params
+    params.require(:order_item).permit(:product_id, :quantity)
+  end
 end
