9ad198f315019d2ae8ee72369b14fd54505b1196
jernber <oraclemars@gmail.com>
2020-04-12 19:05:14 -0500

    setup user creation form

--- whittier/app/controllers/clothing_orders_controller.rb
+++ whittier/app/controllers/clothing_orders_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class ClothingOrdersController < ApplicationController
+  before_action :authenticate_user!
   def create
     @order = current_order
     @order.clothing_orders.new(order_params)
+    @order.clothing_orders.user_id = current_user
     @order.save!
     session[:order_id] = @order.id
   end
