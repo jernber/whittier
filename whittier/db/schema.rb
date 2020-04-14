bfc565d286c4f9dce408756808268b6c2af917ac
jernber <oraclemars@gmail.com>
2020-04-14 16:31:12 -0500

    added province column to user

--- whittier/db/schema.rb
+++ whittier/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_202006) do
+ActiveRecord::Schema.define(version: 2020_04_14_212026) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_202006) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "province"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
