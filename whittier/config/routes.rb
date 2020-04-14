f58309e11db562bcd06d6f2f1b1d0f7c81d114f3
jernber <oraclemars@gmail.com>
2020-04-13 19:19:41 -0500

    added devise user controller and changed route

--- whittier/config/routes.rb
+++ whittier/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   get 'carts/show'
   get 'searches/index'
   get 'contacts/index'
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :searches
   resources :clothing_orders
-  resource :carts, only:[:show]
+  resource :carts, only: [:show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
