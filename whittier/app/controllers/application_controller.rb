2635ac48dc963f27d28ff792c2a702da6f940fde
jernber <oraclemars@gmail.com>
2020-04-14 19:07:40 -0500

    finalized adding provinced to user

--- whittier/app/controllers/application_controller.rb
+++ whittier/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name first_name last_name email phone_number city country address postal_code])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[username name first_name last_name email phone_number city country address postal_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name first_name last_name email phone_number city country address postal_code province])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username name first_name last_name email phone_number city country address postal_code province])
   end
 end
