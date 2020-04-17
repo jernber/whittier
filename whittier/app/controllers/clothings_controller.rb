81e458ad0a03af461998b082ececc994f214fad7
jernber <oraclemars@gmail.com>
2020-04-17 12:34:16 -0500

    added breadcrumbs

--- whittier/app/controllers/clothings_controller.rb
+++ whittier/app/controllers/clothings_controller.rb
@@ -3,7 +3,7 @@
 class ClothingsController < ApplicationController
   before_action :initialize_session
   before_action :load_cart
-
+  add_breadcrumb 'home', :root_path
   def index
     random_number = rand(1..Clothing.count)
     @clothes = Clothing.where(id: random_number)
@@ -27,26 +27,32 @@ class ClothingsController < ApplicationController
 
   def tops
     @clothes = Clothing.where(clothing_type: :top).page(params[:page]).per(30)
+    add_breadcrumb 'tops', clothings_tops_path
   end
 
   def bottoms
     @clothes = Clothing.where(clothing_type: :bottom).page(params[:page]).per(30)
+    add_breadcrumb 'bottoms', clothings_bottoms_path
   end
 
   def accessory
     @clothes = Clothing.where(clothing_type: :accessory).page(params[:page]).per(30)
+    add_breadcrumb 'accessory', clothings_accessory_path
   end
 
   def footwear
     @clothes = Clothing.where(clothing_type: :footwear).page(params[:page]).per(30)
+    add_breadcrumb 'footwear', clothings_footwear_path
   end
 
   def jackets
     @clothes = Clothing.where(clothing_type: :jacket).page(params[:page]).per(30)
+    add_breadcrumb 'jackets', clothings_jackets_path
   end
 
   def show
     @clothes = Clothing.find(params[:id])
+    add_breadcrumb @clothes.name, clothing_path(@clothes.id)
   end
 
   def edit; end
